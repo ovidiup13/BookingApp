55183941ac2dda7141705c54246ce1351a292404
Ovidiu Popoviciu <devovip13@gmail.com>
2018-03-16 00:18:19 +0000

    Add collection name to schema

--- server/db/booking.model.js
+++ server/db/booking.model.js
@@ -2,18 +2,21 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const BookingSchema = new Schema({
-  // define schema here
-  startTime: { type: Date, required: [true, "Start time is required"] },
-  endTime: { type: Date, required: [true, "End time is required"] },
-  service: { type: String, enum: ["haircut", "meeting", "reservation"] },
-  customer: {
-    firstName: String,
-    lastName: String,
-    email: String
+const BookingSchema = new Schema(
+  {
+    // define schema here
+    startTime: { type: Date, required: [true, "Start time is required"] },
+    endTime: { type: Date, required: [true, "End time is required"] },
+    service: { type: String, enum: ["haircut", "meeting", "reservation"] },
+    customer: {
+      firstName: String,
+      lastName: String,
+      email: String
+    },
+    resource: String
   },
-  resource: String
-});
+  { collection: "bookings" }
+);
 
 const BookingModel = mongoose.model("BookingModel", "BookingSchema");
 
