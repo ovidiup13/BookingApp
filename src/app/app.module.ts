5633220f9c95c670118601bc58258dd77f5e29b4
Ovidiu Popoviciu <devovip13@gmail.com>
2018-03-19 14:52:10 +0000

    Add home and not-found components. Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,30 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
+// components
 import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home.component";
+import { NotFoundComponent } from "./not-found.component";
+
+// modules
+import { BookingsModule } from "./bookings/bookings.module";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "**",
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes), BookingsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/not-found.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  template: `
+        <div>
+            <h1>404</h1>
+            <h3>The resource you are looking for does not exist.</h3>
+        </div>
+    `
+})
+export class NotFoundComponent {}
